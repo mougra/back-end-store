a71372a97c7edc5e949250804a74040f14e2d690
egor antrop <mougraaa@gmail.com>
2023-12-14 20:52:16 +0400

    feat: user bd and seeder

--- src/auth/decorators/user.decorator.ts
+++ src/auth/decorators/user.decorator.ts
@@ -4,7 +4,7 @@ import { User } from '@prisma/client';
 export const CurrentUser = createParamDecorator(
   (data: keyof User, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
-    const user = request.User;
+    const user = request.user;
 
     return data ? user[data] : user;
   },
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,45 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Put,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { CurrentUser } from 'src/auth/decorators/user.decorator';
+import { UserDto } from './user.dto';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get('profile')
+  @Auth()
+  async getProfile(@CurrentUser('id') id: number) {
+    console.log(id);
+    return this.userService.byId(id);
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Auth()
+  @Put('profile')
+  async getNewTokens(@CurrentUser('id') id: number, @Body() dto: UserDto) {
+    return this.userService.updateProfile(id, dto);
+  }
+
+  @HttpCode(200)
+  @Auth()
+  @Patch('profile/favorites/:productId')
+  async toggleFavorite(
+    @CurrentUser('id') id: number,
+    @Param('productId') productId: string,
+  ) {
+    return this.userService.toggleFavorite(id, +productId);
+  }
 }
